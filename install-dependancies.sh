f7944fe282b4990e9da46874760c9150b16581e2
Nikolay Shopik <shopik@inblock.ru>
2015-08-26 12:55:54 +0300

    update font package name

--- install-dependancies.sh
+++ install-dependancies.sh
@@ -4,7 +4,7 @@ echo -e "Needed libs to compile \n libsdl1.2-dev libsdl-ttf2.0-dev libsdl-gfx1.2
 sudo apt-get install libsdl1.2-dev libsdl-ttf2.0-dev libsdl-gfx1.2-dev libx11-dev
 echo "Done"
 echo "We'll need droid fonts too. Otherwise it'll crash"
-sudo apt-get install ttf-droid
+sudo apt-get install fonts-droid
 echo "Done"
 echo "You'll need to switch to xscreensaver. Installing."
 sudo apt-get install xscreensaver
